2b8db5c215ea3ef1189015c57fcb78a082deecf3
Amit <amits2194@gmail.com>
2018-08-12 13:09:27 -0700

    added user name in preference during the sign up forced sign in

--- app/src/main/java/com/example/sarve/scoutui/SignUp.java
+++ app/src/main/java/com/example/sarve/scoutui/SignUp.java
@@ -2,6 +2,7 @@ package com.example.sarve.scoutui;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.sarve.scoutui.Model.Globals;
 import com.example.sarve.scoutui.Model.User;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
@@ -71,7 +73,13 @@ public class SignUp extends AppCompatActivity {
                             @Override
                             public void onSuccess(Void aVoid) {
 
+                                //adding usernames to the shared preference so we can access it later : amit J
+                                SharedPreferences.Editor editor = getSharedPreferences(Globals.SCOUT_PREFERENCENAME, MODE_PRIVATE).edit();
+                                editor.putString("username", editPhone.getText().toString());
+                                editor.apply();
 
+
+                                SharedPreferences prefs = getSharedPreferences(Globals.SCOUT_PREFERENCENAME, MODE_PRIVATE);
                                 Intent i = new Intent(SignUp.this, InitialSetup.class);
                                 startActivity(i);
                          mDialog.dismiss();
